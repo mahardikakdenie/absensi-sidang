8aa48003b17bac189534e59079d0daa2da38e0e5
MahardiikaKD <dikamahar884@gmail.com>
2023-11-15 12:53:46 +0700

    face api

---
+++ src/components/Modal/Attandance.vue
@@ -0,0 +1,200 @@
+<template>
+	<modal :active-modal="activeModal" @close="$emit('close')">
+		<div>
+            <p v-if="location">Latitude: {{ location.latitude }}, Longitude: {{ location.longitude }}</p>
+			<div class="relative aspect-w-16 aspect-h-9">
+				<video
+					id="video-webcam"
+					ref="videoElement"
+					autoplay
+					class="object-cover w-full h-full" />
+				<div
+					class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+					<img
+						v-if="imgUrlState"
+						:src="imgUrlState"
+						alt=""
+						class="object-cover w-full h-full" />
+				</div>
+			</div>
+
+			<div class="mt-4">
+				<vue-button
+                    v-if="!imgUrlState"
+					text="Take"
+					btnClass="btn-outline-primary btn-sm"
+					@click="take" 
+                />
+				<vue-button
+                    v-if="imgUrlState"
+					text="Upload"
+					btnClass="btn-outline-primary btn-sm"
+					@click="upload"
+                />
+			</div>
+		</div>
+	</modal>
+</template>
+
+<script setup>
+import VueButton from '@/components/Button';
+import Modal from '@/components/Modal/index.vue';
+import { onBeforeMount, onMounted, ref, watch } from 'vue';
+const props = defineProps({
+	activeModal: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+
+const location = ref(null);
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      handleSuccess,
+      handleError
+    );
+  } else {
+    alert('Geolocation is not supported by your browser.');
+  }
+};
+
+const handleSuccess = (position) => {
+  location.value = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude
+  };
+};
+
+const handleError = (error) => {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert('User denied the request for Geolocation.');
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert('Location information is unavailable.');
+      break;
+    case error.TIMEOUT:
+      alert('The request to get user location timed out.');
+      break;
+    case error.UNKNOWN_ERROR:
+      alert('An unknown error occurred.');
+      break;
+    default:
+      alert('An unknown error occurred.');
+  }
+};
+
+const videoState = ref(null);
+
+const imgState = ref(null);
+const imgUrlState = ref(null);
+
+// ...
+const upload =  () => {};
+const take = () => {
+	if (videoElement.value) {
+		const width = videoElement.value.offsetWidth;
+		const height = videoElement.value.offsetHeight;
+
+		const canvas = document.createElement('canvas');
+		canvas.width = width;
+		canvas.height = height;
+
+		const context = canvas.getContext('2d');
+
+		context.drawImage(videoElement.value, 0, 0, width, height);
+
+		const img = new Image();
+		img.src = canvas.toDataURL('image/png');
+
+		img.onload = () => {
+			imgState.value = img;
+			imgUrlState.value = img.src;
+		};
+	}
+};
+
+const videoElement = ref(null);
+watch(
+	() => props.activeModal, // Mengamati prop activeModal
+	(newVal, oldVal) => {
+		console.log('🚀 ~ file: Attandance.vue:52 ~ newVal:', newVal);
+		// Callback yang akan dijalankan ketika prop berubah
+		if (newVal) {
+			const video =
+				document.getElementById('video-webcam') || videoElement;
+			console.log('🚀 ~ file: Attandance.vue:56 ~ video:', video);
+
+			if (video) {
+				navigator.getUserMedia =
+					navigator.getUserMedia ||
+					navigator.webkitGetUserMedia ||
+					navigator.mozGetUserMedia ||
+					navigator.msGetUserMedia ||
+					navigator.oGetUserMedia;
+
+				if (navigator.getUserMedia) {
+					navigator.getUserMedia(
+						{ video: true },
+						handleVideo,
+						videoError
+					);
+				}
+
+				function handleVideo(stream) {
+					video.srcObject = stream;
+					videoState.value = video;
+				}
+
+				function videoError(e) {
+					alert('Allow using the webcam for the demo!');
+				}
+			}
+		}
+	},
+	{ immediate: true } // Opsi agar watch dijalankan saat komponen di-render pertama kali
+);
+
+onBeforeMount(() => {
+	console.log('🚀 ~ file: Attandance.vue:90 ~ videoElement:', videoElement);
+	if (props.activeModal) {
+		const video = document.getElementById('video-webcam') || videoElement;
+		console.log('🚀 ~ file: Attandance.vue:56 ~ video:', video);
+
+		if (video) {
+			navigator.getUserMedia =
+				navigator.getUserMedia ||
+				navigator.webkitGetUserMedia ||
+				navigator.mozGetUserMedia ||
+				navigator.msGetUserMedia ||
+				navigator.oGetUserMedia;
+
+			if (navigator.getUserMedia) {
+				navigator.getUserMedia(
+					{ video: true },
+					handleVideo,
+					videoError
+				);
+			}
+
+			function handleVideo(stream) {
+				const videoTag = document.getElementById('video-webcam');
+				console.log(
+					'🚀 ~ file: Attandance.vue:112 ~ handleVideo ~ videoTag:',
+					videoTag
+				);
+				videoTag.srcObject = stream;
+				videoState.value = video;
+			}
+
+			function videoError(e) {
+				alert('Allow using the webcam for the demo!');
+			}
+		}
+        getLocation();
+	}
+});
+</script>
--- src/views/pages/detail-project.vue
+++ src/views/pages/detail-project.vue
@@ -1,177 +1,173 @@
 <template>
-<div>
-    <div class="bg-white lg:w-[200px] border rounded-md flex p-2 justify-around">
-        <information-color
-            v-for="(information, index) in formatInformations"
-            :key="index"
-            :color-code="information.colorCode"
-            :label="information.label"
-        />
-    </div>
-    
-    <div class="">
-        <card>
-            <div class="flex justify-between">
-                <div>
-                    <span class="">
-                        CODE -ABC
-                    </span>
-                </div>
-                <div>
-                    <div>
-                        <span class="font-bold text-[12px] lg:text-lg">
-                            Rp 9.999.999.999
-                        </span>
-                    </div>
-                    <div class="flex justify-end">
-                        <span class="text-sm">
-                            01-01-2023
-                        </span>
-                    </div>
-                </div>
-            </div>
+	<div>
+		<div
+			class="bg-white lg:w-[200px] border rounded-md flex p-2 justify-around">
+			<information-color
+				v-for="(information, index) in formatInformations"
+				:key="index"
+				:color-code="information.colorCode"
+				:label="information.label" />
+		</div>
 
-            <single-accordion 
-                title="Project Name"
-                parentClass="mt-3"
-                isOpen
-            >
-                <template #content>
-                    <div class="mb-3">
-                        <span class="font-bold text-lg">
-                            Progress
-                        </span>
-                    </div>
-                    <div>
-                        <span>Progress</span>
-                        <ProgressBar height="h-3" parentClass="mt-2">
-                            <Bar 
-                                :value="80" 
-                                barColor="bg-[#CE7722]" 
-                                showValue 
-                            />
-                            <Bar 
-                                :value="10" 
-                                barColor="bg-[#FFE5B4]" 
-                                showValue
-                                colorTextClass="text-black"
-                            />
-                        </ProgressBar>
-                    </div>
-                    <div class="mt-3">
-                        <span>Deviasi</span>
-                        <ProgressBar height="h-3" parentClass="mt-2">
-                            <Bar 
-                                :value="40" 
-                                barColor="bg-[#CE7722]" 
-                                showValue
-                            />
-                            <Bar 
-                                :value="40" 
-                                barColor="bg-[#FFE5B4]" 
-                                showValue
-                                colorTextClass="text-black"
-                            />
-                        </ProgressBar>
-                    </div>
-                </template>
-            </single-accordion>
-            <single-accordion title="Keterangan" isOpen parentClass="mt-2">
-                <template #content>
-                    <div>
-                        <div>
-                            <span>
-                                Pengawas : Budi Raharja
-                            </span>
-                        </div>
-                        <div>
-                            <span>
-                                Sisa Hari : 61 Hari
-                            </span>
-                        </div>
-                        <div class="flex justify-between">
-                            <div class="flex items-center">
-                                <span class="text-sm font-bold">
-                                    Task
-                                </span>
-                            </div>
-                            <div class="">
-                                <vue-button 
-                                    text="Edit Task" 
-                                    btnClass="py-2 px-2" 
-                                    icon="typcn:edit" 
-                                />
-                            </div>
-                        </div>
-                        <div class="flex flex-wrap space-x-rb mt-4">
-                            <Checkbox
-                                v-for="(option, index) in options"
-                                :key="index"
-                                v-model="selected"
-                                :value="option.value"
-                                :label="option.label"
-                                activeClass="ring-primary-500 bg-primary-500"
-                                class="ml-2"
-                            />
-                        </div>
+		<div class="">
+			<card>
+				<div class="flex justify-between">
+					<div>
+						<span class=""> CODE -ABC </span>
+					</div>
+					<div>
+						<div>
+							<span class="font-bold text-[12px] lg:text-lg">
+								Rp 9.999.999.999
+							</span>
+						</div>
+						<div class="flex justify-end">
+							<span class="text-sm"> 01-01-2023 </span>
+						</div>
+					</div>
+				</div>
 
-
-                    </div>
-                </template>
-            </single-accordion>
-            <div class="mt-4">
-                <div class="text-md font-bold">
-                    Desctipription : 
-                </div>
-                <div class="font-light text-justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, veniam!
-                </div>
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-center gap-2">
-                    <vue-button text="ClockIn" btnClass="btn-outline-primary btn-sm" />
-                    <vue-button text="ClockOut" btnClass="btn-outline-primary btn-sm" />
-                </div>
-            </div>
-        </card>
-    </div>
-</div>
+				<single-accordion
+					title="Project Name"
+					parentClass="mt-3"
+					isOpen>
+					<template #content>
+						<div class="mb-3">
+							<span class="font-bold text-lg"> Progress </span>
+						</div>
+						<div>
+							<span>Progress</span>
+							<ProgressBar height="h-3" parentClass="mt-2">
+								<Bar
+									:value="80"
+									barColor="bg-[#CE7722]"
+									showValue />
+								<Bar
+									:value="10"
+									barColor="bg-[#FFE5B4]"
+									showValue
+									colorTextClass="text-black" />
+							</ProgressBar>
+						</div>
+						<div class="mt-3">
+							<span>Deviasi</span>
+							<ProgressBar height="h-3" parentClass="mt-2">
+								<Bar
+									:value="40"
+									barColor="bg-[#CE7722]"
+									showValue />
+								<Bar
+									:value="40"
+									barColor="bg-[#FFE5B4]"
+									showValue
+									colorTextClass="text-black" />
+							</ProgressBar>
+						</div>
+					</template>
+				</single-accordion>
+				<single-accordion title="Keterangan" isOpen parentClass="mt-2">
+					<template #content>
+						<div>
+							<div>
+								<span> Pengawas : Budi Raharja </span>
+							</div>
+							<div>
+								<span> Sisa Hari : 61 Hari </span>
+							</div>
+							<div class="flex justify-between">
+								<div class="flex items-center">
+									<span class="text-sm font-bold">
+										Task
+									</span>
+								</div>
+								<div class="">
+									<vue-button
+										text="Edit Task"
+										btnClass="py-2 px-2"
+										icon="typcn:edit" />
+								</div>
+							</div>
+							<div class="flex flex-wrap space-x-rb mt-4">
+								<Checkbox
+									v-for="(option, index) in options"
+									:key="index"
+									v-model="selected"
+									:value="option.value"
+									:label="option.label"
+									activeClass="ring-primary-500 bg-primary-500"
+									class="ml-2" />
+							</div>
+						</div>
+					</template>
+				</single-accordion>
+				<div class="mt-4">
+					<div class="text-md font-bold">Desctipription :</div>
+					<div class="font-light text-justify">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Velit, veniam!
+					</div>
+				</div>
+				<div class="mt-4">
+					<div class="flex justify-center gap-2">
+						<vue-button
+							text="ClockIn"
+							btnClass="btn-outline-primary btn-sm"
+							@click="startCamera" />
+						<vue-button
+							text="ClockOut"
+							btnClass="btn-outline-primary btn-sm" />
+					</div>
+				</div>
+			</card>
+		</div>
+        <ModalAttendance v-if="isOpenCamera" :active-modal="isOpenCamera" @close="isOpenCamera = false" />
+	</div>
 </template>
 
 <script setup>
+// import { useMediaRecorder } from 'vue-media-recorder';
 import InformationColor from '@/components/attandance-pages/InformationColor.vue';
 import Card from '@/components/Card';
+import Modal from '@/components/Modal';
 import SingleAccordion from '@/components/Accordion/SingleAccordion.vue';
 import ProgressBar from '@/components/ProgressBar';
 import Bar from '@/components/ProgressBar/Bar';
 import VueButton from '@/components/Button/index.vue';
-import Checkbox from '@/components/Checkbox/index.vue'
-import {
-    formatInformations
-} from '@/constant/static';
-import { computed, ref } from 'vue';
+import Checkbox from '@/components/Checkbox/index.vue';
+import { formatInformations } from '@/constant/static';
+import { computed, onMounted, ref } from 'vue';
 import { useThemeSettingsStore } from '@/store/themeSettings';
+import ModalAttendance from '@/components/Modal/Attandance.vue'
 
 const store = useThemeSettingsStore();
 
+const selected = ref([]);
+
 const options = [
-    {
-        value: "Orange",
-        label: "Task I",
-    },
-    {
-        value: "Apple",
-        label: "Task II",
-    },
-    {
-        value: "Banana",
-        label: "Task III",
-    },
+	{
+		value: 'Orange',
+		label: 'Task I',
+	},
+	{
+		value: 'Apple',
+		label: 'Task II',
+	},
+	{
+		value: 'Banana',
+		label: 'Task III',
+	},
 ];
 
-const selected = ref([]);
+// Ref for video element
+const videoElement = ref(null);
+
+const isOpenCamera = ref(false);
+const videoState = ref(null);
+const imgState = ref(null);
+
+const startCamera = () => {
+	isOpenCamera.value = true;
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
