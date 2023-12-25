147974c9e6674d91bd097aeb9778212cd1121397
Encep Ihwan <ihwanencep@gmail.com>
2023-12-26 06:36:40 +0700

    make read data project

--- src/views/pages/admin/project.vue
+++ src/views/pages/admin/project.vue
@@ -1,16 +1,67 @@
 <template>
-<div>
-    <card>
-        <data-table title="Project" />
-    </card>
-</div>
-</template>
-
-<script setup>
-import Card from '@/components/Card/index.vue';
-import DataTable from '@/components/DataTable/index.vue'
-</script>
-
-<style>
-
-</style>
+    <div>
+        <card>
+            <data-table title="Project" />
+        </card>
+    </div>
+    </template>
+    
+    <script setup>
+    import Card from '@/components/Card/index.vue';
+    import DataTable from '@/components/DataTable/index.vue';
+    import projectsApi from '@/helpers/projects.js';
+    import { useDataTableStore } from '@/store/data-table.js';
+    import { onMounted } from 'vue';
+    
+    const store = useDataTableStore();
+    
+    const headers = [
+        {
+            label: 'Project Number',
+            field: 'projectNo', 
+        },
+        {
+            label: 'Name',
+            field: 'name'
+        },
+        {
+            label: 'Address',
+            field: 'address'
+        },
+        {
+            label: 'Cost',
+            field: 'cost'
+        }, 
+        {
+            label: 'Actions',
+            field: 'actions'
+        }
+    ];
+    
+    const actions = [];
+    
+    const getData = () => {
+        const callback = (response) => {
+            const projects = response?.data?.data;
+            console.log("🚀 ~ file: division.vue:33 ~ callback ~ divisions:", projects);
+            store.setData(projects);
+        };
+    
+        const err = (e) => {
+            console.log('e => ', e);
+        };
+    
+        projectsApi.getData({}, callback, err);
+    };
+    
+    onMounted(() => {
+        getData();
+        store.setHeaders(headers)
+    });
+    
+    </script>
+    
+    <style>
+    
+    </style>
+    
